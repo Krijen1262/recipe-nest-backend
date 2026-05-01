1e2c0197894151d2a8616a77e4dab000b696ebec
Krijen1262 <krijen.shahi101@gmail.com>
2026-05-01 10:36:38 +0545

    fix layout

--- server.js
+++ server.js
@@ -1,39 +1,3 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// require("dotenv").config();
-
-// const authRoutes = require("./routes/authRoutes");
-// const recipeRoutes = require("./routes/recipeRoutes");
-// const chefRoutes = require("./routes/chefRoutes");
-// const adminRecipeRoutes = require("./routes/adminRecipeRoutes");
-// const userRoutes = require("./routes/userRoutes");
-
-// const app = express();
-
-// // 🔥 FIRST: middleware
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// // 🔥 THEN: routes
-// app.use("/api/auth", authRoutes);
-// app.use("/api/recipes", recipeRoutes);
-// app.use("/api/chefs", chefRoutes);
-// app.use("/api/admin", adminRecipeRoutes);
-// app.use("/api/user", userRoutes);
-
-// // DB
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => {
-//     console.log("MongoDB connected");
-//     app.listen(process.env.PORT, () =>
-//       console.log(`Server running on port ${process.env.PORT}`)
-//     );
-//   })
-//   .catch((err) => console.log(err));
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
